41e1304f2179e2d7faeeaf306d3763dc4132b363
Ali <alissaeid@live.co.uk>
2023-05-27 22:54:04 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,15 @@
 import subprocess
 
 def main():
+    # Check if the taskmanager database exists
+    result = subprocess.run(['psql', '-lqt'], capture_output=True, text=True)
+    databases = result.stdout.strip().split('\n')
+    database_names = [line.split('|')[0].strip() for line in databases]
+
+    if 'taskmanager' not in database_names:
+        # Create the taskmanager database
+        subprocess.run(['createdb', 'taskmanager'])
+
     # Execute the psql command to import the SQL file
     subprocess.run(['psql', '-d', 'taskmanager', '-f', 'taskmanager_db.sql'])
 
