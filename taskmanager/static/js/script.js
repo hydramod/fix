3b7f40247dc99d259793d557fcd89b46b5fe090f
Ali <alissaeid@live.co.uk>
2023-05-27 15:50:52 +0000

    Rebuild repository

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+//sidenav initialization
+document.addEventListener('DOMContentLoaded', function() {
+    var sidenav = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
